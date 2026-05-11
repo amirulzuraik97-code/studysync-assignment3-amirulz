5de114821f544cfbf66b138785df2deda7152444
DESKTOP-AZUZU\User <amirulzuraik97@gmail.com>
2026-05-11 16:42:25 +0800

    Fix user model import

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const User = require("./models/User");
+const User = require("./models/user");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
